faff197d7f662c042ee31ea9e5030adcf7c77b09
Negorizont <boltuev01@mail.ru>
2020-02-24 20:33:27 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length == 0) {
+    return 0;
+  }
+  return array.reduce(function(min, current) {
+    return current < min ? current : min;
+  });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length == 0) {
+    return 0;
+  }
+  return array.reduce(function(max, current) {
+    return current > max ? current : max;
+  });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length == 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => (a + b))/array.length;
 }
